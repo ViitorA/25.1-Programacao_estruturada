e9ea5cfe0a559e51326c5ce248153eef0b188e7b
Victor <victoralexsbr@gmail.com>
2025-05-29 18:02:14 -0300

    Desenvolvido um pouco mais do exercício 24

--- 9-tad_ted_e_struct/exercicios-struct-extra/24.c
+++ 9-tad_ted_e_struct/exercicios-struct-extra/24.c
@@ -8,8 +8,8 @@
  * -> XXX(a) Definir a estrutura acima.
  * -> XXX(b) Declarar a variável agenda (vetor) com capacidade de agendar até 100 nomes.
  * -> XXX(c) Definir um bloco de instruções busca por primeiro nome: Imprime os dados da pessoa com esse nome (se tiver mais de uma pessoa, imprime para todas).
- * -> (d) Definir um bloco de instruções busca por mês de aniversário: Imprime os dados de todas as pessoas que fazem aniversário nesse mês.
- * -> (e) Definir um bloco de instruções busca por dia e mês de aniversário: Imprime os dados de todas as pessoas que fazem aniversário nesse dia e mês.
+ * -> XXX(d) Definir um bloco de instruções busca por mês de aniversário: Imprime os dados de todas as pessoas que fazem aniversário nesse mês.
+ * -> XXX(e) Definir um bloco de instruções busca por dia e mês de aniversário: Imprime os dados de todas as pessoas que fazem aniversário nesse dia e mês.
  * -> (f) Definir um bloco de instruções insere pessoa: Insere por ordem alfabética de nome.
  * -> (g) Definir um bloco de instruções retira pessoa: Retira todos os dados dessa pessoa e desloca todos os elementos seguintes do vetor para a posição anterior.
  * -> (h) Definir um bloco de instruções imprime agenda com as opções:
@@ -61,34 +61,41 @@ void imprimir_dados(Pessoa pessoa) {
 	printf("Nome: %s\n", pessoa.nome);
 	printf("E-Mail: %s\n", pessoa.email);
 	printf("Endereço: \n");
-		printf("CEP: %s\n", pessoa.endereco.cep);
-		printf("Rua: %s, num. %d\n", pessoa.endereco.rua, pessoa.endereco.numero);
-		printf("Complemento: %s\n", pessoa.endereco.complemento);
-		printf("Bairro: %s\n", pessoa.endereco.bairro);
-		printf("Cidade: %s\n", pessoa.endereco.cidade);
-		printf("Estado: %s\n", pessoa.endereco.estado);
-		printf("País: %s\n\n", pessoa.endereco.pais);
-	printf("Número de Telefone: ");
-	for(int i = 0; i < 2; i++) {
-		printf("%d", pessoa.telefone.ddd);
-	}
-	printf(" ");
-	for(int i = 0; i < 9; i++) {
-		printf("%d", pessoa.telefone.numero);
-	} 
-	printf("\n");
-	
+		printf("\tCEP: %s\n", pessoa.endereco.cep);
+		printf("\tRua: %s, num. %d\n", pessoa.endereco.rua, pessoa.endereco.numero);
+		printf("\tComplemento: %s\n", pessoa.endereco.complemento);
+		printf("\tBairro: %s\n", pessoa.endereco.bairro);
+		printf("\tCidade: %s\n", pessoa.endereco.cidade);
+		printf("\tEstado: %s\n", pessoa.endereco.estado);
+		printf("\tPaís: %s\n\n", pessoa.endereco.pais);
+	printf("Número de Telefone: %s %s\n", pessoa.telefone.ddd, pessoa.telefone.numero);
 	printf("Aniversário: %d/%d/%d\n", pessoa.aniversario.dia, pessoa.aniversario.mes, pessoa.aniversario.ano);
 	printf("Observações: %s\n", pessoa.observacoes);
 	printf("====================\n");
 }
 
 
-void busca_por_primeiro_nome(char *nome, Pessoa agenda[100]) {
+void busca_por_primeiro_nome(char *nome, Pessoa *agenda) {
 	for(int i = 0; i < 100; i++) {
 		if(strcmp(agenda[i].nome, nome) == 0) {
 			imprimir_dados(agenda[i]);
 		}	
+	}
+}
+
+void busca_mes(int mes, Pessoa *agenda) {
+	for(int i = 0; i < 100; i++) {
+		if(agenda[i].aniversario.mes == mes) {
+			imprimir_dados(agenda[i]);
+		}
+	}
+}
+
+void busca_dia_mes(int dia, int mes, Pessoa *agenda) {
+	for(int i = 0; i < 100; i++) {
+		if(agenda[i].aniversario.mes == mes && agenda[i].aniversario.dia == dia) {
+			imprimir_dados(agenda[i]);
+		}
 	}
 }
 
@@ -113,7 +120,7 @@ void inserir_pessoa(Pessoa agenda[100]) {
 
 		// segfault
 		puts("Inserir número: ");
-		scanf("%d", agenda[i].endereco.numero);
+		scanf("%d", &agenda[i].endereco.numero);
 
 		puts("Inserir complemento: ");
 		scanf("%s", agenda[i].endereco.complemento);
@@ -130,43 +137,58 @@ void inserir_pessoa(Pessoa agenda[100]) {
 		puts("Inserir País: ");
 		scanf("%s", agenda[i].endereco.pais);
 
-		// Depois mudar esse input para o método com char
+		// Limpa o buffer
+		while (getchar() != '\n');
+
 		puts("Insira o número de telefone no formato \"dd nnnnnnnnn\"");
-		scanf("%d%d %d%d%d%d%d%d%d%d%d",
-				agenda[i].telefone.ddd[0],
-				agenda[i].telefone.ddd[1],
-				agenda[i].telefone.numero[0],
-				agenda[i].telefone.numero[1],
-				agenda[i].telefone.numero[2],
-				agenda[i].telefone.numero[3],
-				agenda[i].telefone.numero[4],
-				agenda[i].telefone.numero[5],
-				agenda[i].telefone.numero[6],
-				agenda[i].telefone.numero[7],
-				agenda[i].telefone.numero[8]);
-	
+		char telefone_str[12];
+		fgets(telefone_str, sizeof(telefone_str), stdin);
+		sscanf(telefone_str, "%2s %9s", agenda[i].telefone.ddd, agenda[i].telefone.numero);
+
+		// Limpa o buffer
+		while (getchar() != '\n');
+		
+		puts("Insira o aniversário no formado DD/MM/AAAA: ");
+		scanf("%d/%d/%d", &agenda[i].aniversario.dia, &agenda[i].aniversario.mes, &agenda[i].aniversario.ano);
+
+
+		// Depois arrumar para poder aceitar nenhuma observação
+		puts("Insira alguma observação especial: ");
+		scanf("%s", agenda[i].observacoes);
 	}
 }
 
-void menu(Pessoa *agenda) {
+int menu(Pessoa *agenda) {
 	int user_choice;
 	
 	while(1) {	
-		printf("O QUE DESEJA FAZER? \n");
-		printf("[1] inserir pessoa nova na agenda\n");
-		printf("[2] Buscar pessoa por nome\n");
+		puts("O QUE DESEJA FAZER? \n");
+		puts("[0] SAIR\n");
+		puts("[1] inserir pessoa nova na agenda\n");
+		puts("[2] Buscar pessoa por nome\n");
+		puts("[3] Buscar pessoa por mês de aniversário\n");
+		puts("[4] Buscar pessoa por dia e mês de aniversário\n");
 
 		scanf("%d", &user_choice);
-		switch(user_choice) {
-			case 1:
-				inserir_pessoa(agenda);
-				break;
-			case 2:
-				puts("Qual é o nome que você procura? ");
-				String nome;
-				scanf("%s", nome);
-				busca_por_primeiro_nome(nome, agenda);
-				break;
+		if(user_choice == 0) {
+			return 0;
+		} else if(user_choice == 1) {
+			inserir_pessoa(agenda);
+		} else if(user_choice == 2) {
+			puts("Qual é o nome que você procura? ");
+			String nome;
+			scanf("%s", nome);
+			busca_por_primeiro_nome(nome, agenda);
+		} else if(user_choice == 3) {
+			puts("Qual é o mês que você procura? ");
+			int mes;
+			scanf("%d", &mes);
+			busca_mes(mes, agenda);
+		} else if(user_choice == 4) {
+			puts("Insira o dia e mês que você procura no formato dd/mm: ");
+			int dia, mes;
+			scanf("%d/%d", &dia, &mes);
+			busca_dia_mes(dia, mes, agenda);
 		}
 	}
 }	
